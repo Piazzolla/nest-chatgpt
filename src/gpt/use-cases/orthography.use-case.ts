4a7da23df46d3da7d8ff963280cd6d9d159246fb
Mariano <macacozzz@hotmail.com>
2024-01-30 09:44:21 -0300

    ajustando el prompt

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -10,16 +10,32 @@ export const orthographyCheckUseCase = async( openai:OpenAI, options: Options) =
     const completion = await openai.chat.completions.create({
         messages: [{ 
             role: "system", 
-            content: "You are a helpful assistant." 
+            content: `Te serán provistos textos en españool con posibles errores ortográficos y gramaticales,
+            Las palabras usadas deben existir en el diccionario de la Real Academia Española,
+            Debes responder en formato JSON,
+            tu tarea es corregirlos y retornar la información soluciones,
+            también debes de dar un porcentaje de acierto por el usuario,
+            Si no hay errores, debes retornar un mensaje de felicitaciones.
+            
+            Ejemplo de salida:
+            {
+                userSCore: number,
+                errors: string[], //['error -> solucion']
+                message: string, // Usa emojis para felicitar al usuario
+            }
+            `  
         },
         {
             role:"user",
             content: prompt,
         }
-        ],
-        model: "gpt-3.5-turbo",
+    ],
+        temperature: 0.5,
+        max_tokens: 150,
+        model: "gpt-4",
       });
     
       console.log(completion);
-      return completion.choices[0];
+      const jsonResp = JSON.parse(completion.choices[0].message.content)
+      return jsonResp;
 }
